a9ad2a27115acd5cb3817f79b648e0e730a65632
ParveshSandila <40619318+ParveshSandila@users.noreply.github.com>
2021-10-12 12:16:21 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/techabet_task/MainActivity.java
+++ app/src/main/java/com/example/techabet_task/MainActivity.java
@@ -31,13 +31,10 @@ public class MainActivity extends AppCompatActivity {
         recyclerView=(RecyclerView)findViewById(R.id.recyclerView);
         databaseHelper=new DatabaseHelper(getApplication());
 
+        checkPermission();
+
         LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getApplication(),LinearLayoutManager.VERTICAL,false);
         recyclerView.setLayoutManager(linearLayoutManager);
-
-        getListData();
-
-        PeriodicWorkRequest locationRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 30, TimeUnit.MINUTES).build();
-        WorkManager.getInstance().enqueue(locationRequest);
     }
 
     private void getListData(){
@@ -46,5 +43,47 @@ public class MainActivity extends AppCompatActivity {
         locationListAdapter=new LocationListAdapter(getApplication(),list);
         recyclerView.setAdapter(locationListAdapter);
         Toast.makeText(getApplication(),"Total List Size: "+list.size(),Toast.LENGTH_SHORT).show();
+    }
+
+    // Function to check and request permission
+    private void checkPermission() {
+        if(ContextCompat.checkSelfPermission(getApplication(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 101);
+        }else{
+            turnGPSOn();
+            getListData();
+            PeriodicWorkRequest locationRequest = new PeriodicWorkRequest.Builder(MyWorker.class, 30, TimeUnit.MINUTES).build();
+            WorkManager.getInstance().enqueue(locationRequest);
+        }
+    }
+
+    private void turnGPSOn(){
+        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+        if(!provider.contains("gps")){ //if gps is disabled
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            sendBroadcast(poke);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 101) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Location Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Location Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
